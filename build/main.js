ee56dc1b6b9534db991901429c7c8e556a446f26
DimaGashko <dgashko@mail.ua>
2018-02-07 21:03:29 +0200

    addWatch

--- build/main.js
+++ build/main.js
@@ -91,10 +91,20 @@ document.body.innerHTML = menu;
 /* harmony default export */ __webpack_exports__["a"] = (function(arr, className) {
    return `
       <ul class="${className}">
-         ${arr.map((item) => `<li>${item}</li>`).join('')}
+         ${arr.map((item) => `<li style="color: ${randColor()}">${item}</li>`).join('')}
       </ul>
    `
 });
+
+function randColor() { 
+   var colors = ['red', 'green', 'blue'];
+   var w = Math.random();
+   
+   if (w < .33) return colors[0];
+   else if (w < .66) return colors[1];
+   
+   return colors[2]; 
+} 
 
 /***/ })
 /******/ ]);
--- source/menu.js
+++ source/menu.js
@@ -1,7 +1,17 @@
 export default function(arr, className) {
    return `
       <ul class="${className}">
-         ${arr.map((item) => `<li>${item}</li>`).join('')}
+         ${arr.map((item) => `<li style="color: ${randColor()}">${item}</li>`).join('')}
       </ul>
    `
 }
+
+function randColor() { 
+   var colors = ['red', 'green', 'blue'];
+   var w = Math.random();
+   
+   if (w < .33) return colors[0];
+   else if (w < .66) return colors[1];
+   
+   return colors[2]; 
+} 
