9fbefb9d91859d0045f00ae2e671bcfd309ed4ba
DimaGashko <dgashko@mail.ua>
2019-01-21 09:41:23 +0200

    clean

--- source/components/menu/menu.js
+++
@@ -1,9 +0,0 @@
-import './menu.sass';
-
-export default function(arr, className) {
-   return `
-      <ul class="menu ${className}">
-         ${arr.map((item) => `<li class="menu__item">${item}</li>`).join('')}
-      </ul>
-   `
-}
--- source/pages/base.js
+++
@@ -1,3 +0,0 @@
-import './base.sass';
- 
-console.log('base js');
