7d46dc6f2bb93bb7fc68540e169c7796564f295b
DimaGashko <dgashko@mail.ua>
2019-01-21 19:49:17 +0200

    sourcemap

--- webpack.config.js
+++ webpack.config.js
@@ -6,10 +6,12 @@ const webpackPlugins = {
 }
 
 module.exports = {
+   mode: 'development',
    entry: {
       app: './src/index.js',
       print: './src/print.js',
    },
+   devtool: 'inline-source-map',
    output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
