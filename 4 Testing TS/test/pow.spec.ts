c6507dd2b326998afdef297df15615721ca8a22f
DimaGashko <dgashko@mail.ua>
2019-01-24 16:43:28 +0200

    changeSpec

--- 4 Testing TS/test/pow.spec.ts
+++ 4 Testing TS/test/pow.spec.ts
@@ -1,4 +1,4 @@
-import pow from "../src/pow";
+import myPow from "../src/pow";
 import { expect } from "chai";
 
 describe("Pow", () => {
@@ -6,10 +6,10 @@ describe("Pow", () => {
    for (let i = 0; i < 10; i++) { 
 
       for (let j = 0; j < 10; j++) { 
-         const res = pow(i, j);
+         const res = myPow(i, j);
          const expectRes = Math.pow(i, j);
 
-         it(`${i}^${j} => ${expectRes}`, () => {
+         it(`${i}^${j} = ${expectRes}`, () => {
             expect(res).to.be.equal(expectRes);
          }); 
 
