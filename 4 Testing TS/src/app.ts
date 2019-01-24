7ca196575784a662381ea401498f74a55a0b95c4
DimaGashko <dgashko@mail.ua>
2019-01-24 20:36:20 +0200

    clean

--- 4 Testing TS/src/app.ts
+++ 4 Testing TS/src/app.ts
@@ -1,3 +1,22 @@
 import pow from "./pow";
 
 console.log(`5^3 = ${pow(5, 3)}`);
+
+/**
+ * djafksdfj 
+ * ajsdfklj aksdlfja;skd lf
+ * @class
+ */
+class TestClass {
+   /**
+    * asdjf kasdkflas d;kflsdj
+    * @param a jadsfk ;sldjf;kasl
+    * @param b asdfj klasdjf;la
+    */
+   constructor(private a: number, public b: string) { 
+
+
+   }
+}
+
+new TestClass(5, "Hello");
--- 4 Testing TS/src/pow.ts
+++ 4 Testing TS/src/pow.ts
@@ -1,3 +1,9 @@
+/**
+ * фыо алдо фывжладфоыв жалдофывжлфд
+ * @param n фыовалдыфв оажфлывдао 
+ * @param to офывалдо ыфвжлдаофыжвлд аоыв
+ * @returns asjdfklasdjfk;asldjf;kasjdfkl;sd
+ */
 export default function pow(n: number, to: number): number { 
    return Math.pow(n, to);
 } 
