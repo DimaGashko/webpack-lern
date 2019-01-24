cebef817d0a9ccbdf10d3e9710560a4b49b6d0ca
DimaGashko <dgashko@mail.ua>
2019-01-24 10:45:44 +0200

    webpack.config.js

---
+++ 4 Testing TS/webpack.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+   entry: "./src/app.ts",
+   output: {
+      filename: "dist/bundle.js",
+      resolve: {
+         extensions: [".ts", ".js", ".tsx", ".jsx", ""],
+      }
+   },
+   module: {
+      loaders: [{
+         test: /\tsx?$/,
+         exclude: /node_modules/,
+         loader: "ts-loader",
+      }]
+   }
+}
